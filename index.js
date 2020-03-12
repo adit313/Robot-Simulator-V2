da2cf823389999ca1c051baaf47f851a482b08b8
adit313 <app2116@gmail.com>
2020-03-12 11:05:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,76 @@
 document.addEventListener("DOMContentLoaded", function(){
   createGrid()
   renderBot(currentPosition)
+  const movesContainer = document.getElementById("moves-container")
+  const moveBtn = document.getElementById("move-button")
+  let movesArray =[]
+
+  document.addEventListener("keyup", delete_was_pressed);
+  moveBtn.addEventListener("click", execute_moves);
+
+  function delete_was_pressed(event) {
+    let key_direction
+    console.log(event.key)
+
+
+    switch (event.key) {      
+      case "ArrowUp":
+        key_direction = "up"
+        break;
+      case "ArrowDown":
+        key_direction = "down"
+        break;
+      case "ArrowLeft":
+        key_direction = "left"
+        break;
+      case "ArrowRight":
+        key_direction = "right"
+        break;
+      case "Backspace":
+        key_direction = "delete"
+        console.log("delete")
+        movesArray.pop()
+        movesContainer.children.item(movesArray.length).remove()
+        return;
+      default:
+        break;
+    }
+    let new_event = document.createElement("LI")
+    new_event.innerText = key_direction
+    movesContainer.append(new_event)
+    movesArray.push(key_direction)
+    }
+
+  function execute_moves(event) {
+    console.log("moves")
+    movesArray.forEach(element => {
+      setTimeout(function() {
+        move(element);
+    }, 500);
+    });
+  }
 
   // ADD CODE HERE!
 })
+
+
+
+// switch (event.key) {
+//   case "w":
+//     move("up");
+//     key_direction = "up"
+//     break;
+//   case "s":
+//     move("down");
+//     key_direction = "down"
+//     break;
+//   case "a":
+//     move("left");
+//     key_direction = "left"
+//     break;
+//   case "d":
+//     move("right");
+//     key_direction = "right"
+//     break;
+//   default:
+//     break;
